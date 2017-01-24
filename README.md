ccf9f35839b53577f6f32c7d896baceccb68c2cc
Stephen Kempin <info@stephenkempin.co.uk>
2017-01-24 12:35:41 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# jQuery Text Fade Delay
+Fade in a series of blockquote elements followed by a delayed cite elements, and simultaneously fade both out.
+
+## Demo
+[jQuery Text Fade Delay - Live DEMO] (https://github.com/SKempin/jQuery-text-fade-delay)
+
+
+## Installation
+[node.js](http://nodejs.org/download/) is required to get ``npm``.
+
+If you would like to download the code and try it for yourself:
+
+1. Clone the repo: `https://github.com/SKempin/jQuery-text-fade-delay.git`
+2. `cd /jQuery-text-fade-delay`
+2. Install NPM packages: `npm install`
+3. Build project and launch: `gulp`
+4. Distilled files will be compiled into the `dist` directory
+
+
+## Setup
+
+1. Include the plugin dist CSS and JS files in your `<head>`:
+
+	```html
+	<link rel="stylesheet" href="dist/css/fader.min.css">
+	<script src="dist/js/fader.min.js"></script>
+	```
+
+2. Use the following HTML structure:
+
+    ```html
+        <ul id="quotes">
+            <li>
+                <blockquote>Quote goes here</blockquote>
+                <cite>Citation goes here</cite>
+            </li>
+            <li>
+                <blockquote>Second quote goes here</blockquote>
+                <cite>Citation goes here</cite>
+            </li>
+        </ul>
+    ```
+
+3. Call the plugin:
+
+    ```javascript
+        $("#quotes").fader({
+    	    fadeSpeed: 750,     // blockquote fade in speed
+            duration: 4000,     // duration before fading to the next blockquote
+            citeDelay: 1500,    // delay until cite element fades in
+            citeFadeSpeed: 1000 // cite fade in speed
+    	})
+
+    ```
+
+## Options
+
+Setting | Default Value | Description |
+------------ | ------------ |------------ |
+fadeSpeed | `500` | Blockquote fade in speed (ms)
+duration | `2000` | Duration (ms) before fading to the next blockquote
+citeDelay | `1200` | Delay (ms) until cite element fades in
+citeFadeSpeed | `750` | Cite fade in speed (ms)
+
+
+## Browser Support
+
+- Chrome 42+
+- Firefox 39+
+- Safari 9+
+- Internet Explorer 10+
+- Edge 14+
+
+
+
+## Author
+[Stephen Kempin](http://www.stephenkempin.co.uk)
+
+## Licence
+MIT
---
+++ src/js/fader.js
@@ -0,0 +1,92 @@
+var $ = require('jquery');
+global.jQuery = require("jquery");
+window.jQuery = $;
+ 
+(function($) {
+    $.fn.fader = function(options) {
+
+        // default settings
+        var settings = $.extend({
+            fadeSpeed: 500,
+            duration: 2000,
+            citeDelay: 1200,
+            citeFadeSpeed: 750
+        }, options);
+
+        /**
+         * Fades out element `toFade`, fading in `toFade+1`
+         * If `toFade` is the last element, then the first element will be
+         * faded in.
+         *
+         * @param {Integer} toFade which element to fade out
+         * @param {Array} testimonial array of jQuery elements
+         */
+        function fade(toFade, testimonial, cite) {
+
+            // fade OUT element to opacity 0
+            testimonial[toFade].animate({
+                opacity: 0
+            }, settings.fadeSpeed);
+            cite[toFade].animate({
+                opacity: 0
+            }, settings.fadeSpeed);
+
+            var toShow = toFade === (testimonial.length - 1) ? 0 : toFade + 1;
+
+            // fade IN element to opacity 1
+            testimonial[toShow].animate({
+                opacity: 1
+            }, settings.fadeSpeed);
+            cite[toShow].stop(true, true).delay(settings.citeDelay).animate({
+                opacity: 1
+            }, settings.citeFadeSpeed);
+
+            // set duration of element
+            setTimeout(function() {
+                fade(toShow, testimonial, cite);
+            }, settings.duration);
+
+        };
+
+
+        // find li, add to text array and hide them
+        var testimonial = [];
+        $(this).find('li blockquote').each(function() {
+            testimonial.push($(this));
+            $(this).css({
+                opacity: 0
+            });
+        });
+
+
+        // find li, add to text array and hide them
+        var cite = [];
+        $(this).find('li cite').each(function() {
+            cite.push($(this));
+            $(this).css({
+                opacity: 0
+            });
+        });
+
+
+        // fade in first item in array
+        testimonial[0].animate({
+            opacity: 1
+        }, settings.fadeSpeed / 2);
+
+
+        // run function
+        setTimeout(function() {
+            fade(0, testimonial, cite);
+        }, settings.duration);
+
+
+
+        cite[0].stop(true, true).delay(settings.citeDelay / 2).animate({
+            opacity: 1
+        }, settings.fadeSpeed / 2);
+
+
+    };
+
+}(jQuery));
